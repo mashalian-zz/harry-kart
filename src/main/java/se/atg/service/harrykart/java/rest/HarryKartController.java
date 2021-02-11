453a875ce4e64d1015176320685214f3083e5749
Amir Mashalian <amir.reza.mashalian@telenor.se>
2021-02-11 20:07:00 +0100

    removed unnecessary files and dependencies and make the project to be built

---
+++ src/main/java/se/atg/service/harrykart/java/rest/HarryKartController.java
@@ -0,0 +1,17 @@
+package se.atg.service.harrykart.java.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/java/api")
+public class HarryKartController {
+
+    @PostMapping(path = "/play", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String playHarryKart() {
+        return "{ \"message\": Don't know how to play yet }";
+    }
+
+}
---
+++ src/test/java/se/atg/service/harrykart/java/HarryKartAppTest.java
@@ -0,0 +1,18 @@
+package se.atg.service.harrykart.java;
+
+import java.net.URI;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("java-test")
+public class HarryKartAppTest {
+
+    private final static URI harryKartApp = URI.create("http://localhost:8181/java/api/play");
+
+}
