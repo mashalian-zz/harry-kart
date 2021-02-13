7f9adaddc94c42a5bc3f3065559a2cdec08f4e67
Amir Mashalian <amir.reza.mashalian@telenor.se>
2021-02-13 19:47:45 +0100

    refactoring in unit tests

--- src/test/java/se/atg/service/harrykart/java/HarryKartServiceTest.java
+++ src/test/java/se/atg/service/harrykart/java/HarryKartServiceTest.java
@@ -9,28 +9,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import se.atg.service.harrykart.java.model.request.HarryKartRequest;
 import se.atg.service.harrykart.java.model.response.HarryKartResponse;
 import se.atg.service.harrykart.java.service.HarryKartService;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ActiveProfiles("java-test")
-public class HarryKartAppTest {
+public class HarryKartServiceTest {
 
   private final static URI harryKartApp = URI.create("http://localhost:8181/java/api/play");
   private final static String TIME_TO_BE_LUCKY = "TIMETOBELUCKY";
   private final static String CARGO_DOOR = "CARGO DOOR";
   private final static String HERCULES_BOKO = "HERCULES BOKO";
   private final static String WAIKIKI_SILVIO = "WAIKIKI SILVIO";
+
   private HarryKartService harryKartService;
 
   @BeforeEach
@@ -44,10 +38,13 @@ public class HarryKartAppTest {
 
     HarryKartResponse response = harryKartService.play(harryKartRequest);
     assertNotNull(response);
-    assertEquals(response.getRankings().size(), 3);
+    assertEquals(3, response.getRankings().size());
     assertEquals(TIME_TO_BE_LUCKY, response.getRankings().get(0).getHorse());
+    assertEquals(1, response.getRankings().get(0).getPosition());
     assertEquals(HERCULES_BOKO, response.getRankings().get(1).getHorse());
+    assertEquals(2, response.getRankings().get(1).getPosition());
     assertEquals(CARGO_DOOR, response.getRankings().get(2).getHorse());
+    assertEquals(3, response.getRankings().get(2).getPosition());
   }
 
   @Test
@@ -56,10 +53,13 @@ public class HarryKartAppTest {
 
     HarryKartResponse response = harryKartService.play(harryKartRequest);
     assertNotNull(response);
-    assertEquals(response.getRankings().size(), 3);
+    assertEquals(3, response.getRankings().size());
     assertEquals(WAIKIKI_SILVIO, response.getRankings().get(0).getHorse());
+    assertEquals(1, response.getRankings().get(0).getPosition());
     assertEquals(TIME_TO_BE_LUCKY, response.getRankings().get(1).getHorse());
+    assertEquals(2, response.getRankings().get(1).getPosition());
     assertEquals(HERCULES_BOKO, response.getRankings().get(2).getHorse());
+    assertEquals(3, response.getRankings().get(2).getPosition());
   }
 
   @Test
@@ -68,10 +68,13 @@ public class HarryKartAppTest {
 
     HarryKartResponse response = harryKartService.play(harryKartRequest);
     assertNotNull(response);
-    assertEquals(response.getRankings().size(), 3);
+    assertEquals(3, response.getRankings().size());
     assertEquals(HERCULES_BOKO, response.getRankings().get(0).getHorse());
+    assertEquals(1, response.getRankings().get(0).getPosition());
     assertEquals(TIME_TO_BE_LUCKY, response.getRankings().get(1).getHorse());
+    assertEquals(2, response.getRankings().get(1).getPosition());
     assertEquals(WAIKIKI_SILVIO, response.getRankings().get(2).getHorse());
+    assertEquals(3, response.getRankings().get(2).getPosition());
   }
 
   private HarryKartRequest getHarryKartRequest(String source) throws IOException {
