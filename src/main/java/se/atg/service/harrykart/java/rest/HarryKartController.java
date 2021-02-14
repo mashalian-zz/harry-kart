112b5196e4f7df840e6cb52696bd4ea387feaf66
Amir Mashalian <amir.reza.mashalian@telenor.se>
2021-02-14 21:34:47 +0100

    removed unused imports

--- src/main/java/se/atg/service/harrykart/java/rest/HarryKartController.java
+++ src/main/java/se/atg/service/harrykart/java/rest/HarryKartController.java
@@ -1,12 +1,9 @@
 package se.atg.service.harrykart.java.rest;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.atg.service.harrykart.java.model.request.HarryKartRequest;
